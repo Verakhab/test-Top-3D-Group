e7ab769bdcbd72a83f10d34f7ff708f7604e6fcf
Denis <47863172+Verakhab@users.noreply.github.com>
2021-02-09 13:56:02 +0300

    Update 2021-02-09T10:51:03.434Z

--- gulp/fonts.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-
-module.exports = function fonts() {
-  return gulp.src('./styles/vendor/fonts/*.{eot,ttf,woff,woff2}')
-    .pipe(gulp.dest('build/vendor/fonts/'))
-}
